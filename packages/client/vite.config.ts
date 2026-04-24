60eb7464a80dd86783f348459fe6aca2f043ce02
gamazyn <gamazyn@live.com>
2026-04-24 10:27:22 -0300

    fix: allowedHosts true (boolean) no Vite

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     port: 5173,
     host: true,
-    allowedHosts: 'all',
+    allowedHosts: true,
     proxy: {
       '/api': 'http://localhost:3000',
       '/media': 'http://localhost:3000',
